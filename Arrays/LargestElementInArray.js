eab322808cf56b8cf8cfc5028efe6bd06f4ee6a4
Sai Mounika Peri <saimounikaperi@gmail.com>
2023-07-12 20:34:36 +0530

    Starting with Arrays

---
+++ Arrays/LargestElementInArray.js
@@ -0,0 +1,16 @@
+/**
+Find the Largest element in an array
+Problem Statement: 
+Given an array, we have to find the largest element in the array.
+Sample Input 1:
+6
+4 7 8 6 7 6 
+Sample Output 1:
+8
+Explanation Of Sample Input 1:
+The answer is 8.
+From {4 7 8 6 7 6}, 8 is the largest element.
+----------------------------------------------------------------------------
+Solution-1: Brute Force Approach
+---------------------------------------------------------------------------
+ */
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const dsaPatterns = require('./handlers/patterns');
+const dsaArrays = require('./handlers/arrays');
 
 const express = require("express")
 const app = express()
@@ -7,4 +8,5 @@ response.send("Hello World!")
 })
 app.listen(10000, function () {
   dsaPatterns.patterns();
+  dsaArrays.arrays();
 });
